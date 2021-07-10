e9ebc401e16d73c183e00990b3e485dd746112eb
Nikita213-hub <70839622+Nikita213-hub@users.noreply.github.com>
2021-07-10 13:45:23 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+       this.res = 0;
+       this.max = 0;
+       this.min = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+        this.res = (this.min + this.max)/2;	    
+        return Math.ceil(this.res);
     }
 
     lower() {
-
+        this.max = Math.ceil(this.res);
     }
 
     greater() {
-
+        this.min = Math.ceil(this.res);
     }
 }
 
